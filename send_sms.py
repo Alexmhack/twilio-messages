2ef62bf87b8732e55cbbe8296a0fa3e2b6e2c91c
alexHooper <alexmohack@gmail.com>
2018-09-16 16:37:41 +0530

    add twillio credentials

--- send_sms.py
+++ send_sms.py
@@ -1,3 +1,12 @@
+import os
+
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+twillio_number = os.getenv("TWILLIO_NUMBER")
+
+message_body = "This is a twillio message."
+
+base_url = "https://api.twilio.com/2010-04-01/Accounts"
+
