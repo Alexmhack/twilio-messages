b11a3eaf78235799f52d0794e2e6a76ee4dd01eb
alexHooper <alexmohack@gmail.com>
2018-09-16 16:59:48 +0530

    add account_sid and account_token in file

--- send_sms.py
+++ send_sms.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+account_sid = os.getenv("TWILLIO_SID")
+account_token = os.getenv("TWILLIO_TOKEN")
+
 twillio_number = os.getenv("TWILLIO_NUMBER")
 
 message_body = "This is a twillio message."
 
 base_url = "https://api.twilio.com/2010-04-01/Accounts"
-
